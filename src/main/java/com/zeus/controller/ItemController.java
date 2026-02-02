65e0137621f22eb52071d68d49575890aa92bb48
domino3333 <busster@naver.com>
2026-02-02 10:20:04 +0900

    프로젝트 세팅 완료

---
+++ src/main/java/com/zeus/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.zeus.controller;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zeus.service.ItemService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping("/item")
+@Controller
+@MapperScan(basePackages = "com.zeus.mapper")
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	
+	
+	
+	
+}
